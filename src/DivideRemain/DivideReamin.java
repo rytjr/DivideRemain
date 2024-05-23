a0e3e5ae0c7131adf71785d3fea455ad31cb7216
kjk71 <kks007159@nqver.com>
2024-05-23 15:30:09 +0900

    Didvide and Conquer

---
+++ src/DivideRemain/DivideReamin.java
@@ -0,0 +1,34 @@
+package DivideRemain;
+
+import java.util.Scanner;
+
+public class DivideReamin {
+ 
+	public static long C;
+	
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		long A = in.nextLong();
+		long B = in.nextLong();
+		C = in.nextLong();
+		
+		System.out.println(pow(A, B));
+	}
+	
+	public static long pow(long A, long exponent) {
+		
+		if(exponent == 1) {
+			return A % C;
+		}
+		
+		long temp = pow(A, exponent / 2);s
+		if(exponent % 2 == 1) {
+			return (temp * temp % C) * A % C;
+		}
+		return temp * temp % C;
+		
+	}
+	
+}
